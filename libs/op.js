83e496318440eafc3fff0352f15ed2394125c954
Hilcbek <balemayehu07@gmail.com>
2024-03-05 21:11:03 +0300

    frontend added 5

--- libs/op.js
+++ libs/op.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export let baseUrl = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ecommerce-backend-ten-beta.vercel.app/api",
   withCredentials: true,
 });
 // https://ecommerce-backend-ten-beta.vercel.app
