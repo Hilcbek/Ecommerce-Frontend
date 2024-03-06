807a63f5b07e9beb00c0df97dc51197473903a66
Hilcbek <balemayehu07@gmail.com>
2024-03-06 21:37:55 +0300

    last final-+3

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -42,7 +42,7 @@ const Product = ({ product }) => {
     }
   };
   return (
-    <div className="w-full flex items-center group justify-between flex-col h-[380px] bg-white relative border-solid border-[1px] hover:shadow-xl hover:border-transparent border-gray-200 cursor-pointer overflow-hidden transition-all duration-300 ease-linear group">
+    <div className="w-full flex items-center group justify-between flex-col h-[390px] bg-white relative border-solid border-[1px] hover:shadow-xl hover:border-transparent border-black cursor-pointer overflow-hidden group">
       <div
         className={`gap-1 z-[99999999] w-fit transition-all duration-200 ease-linear flex absolute top-1 group-hover:right-0 xl:-right-52 right-1 items-center justify-center flex-col`}
       >
@@ -82,15 +82,15 @@ const Product = ({ product }) => {
       </div>
       <Link
         to={`/product/${product?._id}`}
-        className="w-full h-[calc(100%-140px)] group-hover:h-[calc(100%-140px)]"
+        className="w-full h-[calc(100%-150px)] group-hover:h-[calc(100%-150px)]"
       >
         <img src={image} className="w-full h-full object-contain" alt="" />
       </Link>
-      <div className="md:flex hidden w-full md:group-hover:flex items-start gap-1 -mb-5 px-1 justify-start">
+      <div className="md:flex overflow-x-scroll overflow-y-hidden hidden w-full md:group-hover:flex items-start gap-1 -mb-5 px-1 justify-start">
         {product?.img?.map((c, idx) => (
           <div
             key={idx}
-            className="w-14 h-14 border-solid p-[1px] bg-white/50 hover:border-[0px] hover:border-b-[1px] border-[1px] mb-3 border-transparent hover:border-b-black transition duration-200 ease-linear"
+            className="w-11 h-11 border-solid p-[1px] bg-white/50 hover:border-b-[1px] border-b-[1px] mb-5 hover:border-b-black"
           >
             <img
               onMouseOver={handleHover}
@@ -103,24 +103,30 @@ const Product = ({ product }) => {
         ))}
       </div>
       <div className="bg-gray-50 relative w-full py-2 pl-2 flex items-start justify-start flex-col md:gap-0 gap-3">
-        <div className="bg-gray-50 shadow-sm absolute -top-3 left-1 text-xs rounded-sm px-2 p-1">
-          <span className="line-through">${product?.old_price}</span>
-          <span className="ml-2 font-bold">${product?.new_price}</span>
+        <div className="flex w-full items-center px-2 justify-between gap-2 my-2 colorScroll">
+          <div className="flex items-center justify-start gap-2">
+            {product?.colors?.map((color) => (
+              <div
+                onClick={() => setOptions({ ...options, ColorChoice: color })}
+                key={color}
+                className={`${
+                  options.ColorChoice.length > 0 &&
+                  options.ColorChoice === color &&
+                  "scale-[1.4]"
+                } w-7 md:w-4 h-7 md:h-4 rounded-full active:scale-[.93] cursor-pointer before:absolute before:content-[""] md:before:w-5 md:before:h-5 before:h-8 before:w-8 before:border-solid before:border-[1.4px] before:rounded-full before:-top-[2px] before:-left-[2px] before:border-gray-300 relative`}
+                style={{ backgroundColor: color, borderColor: color }}
+              ></div>
+            ))}
+          </div>
+          <div className="bg-gray-50 border-solid border-black border-[1px] text-xs px-2 p-1">
+            {product?.old_price > 0 && (
+              <span className="line-through">${product?.old_price}</span>
+            )}
+            <span className="ml-2 font-bold">${product?.new_price}</span>
+          </div>
         </div>
-        <h1 className="font-Roboto font-semibold">{product.title}</h1>
-        <div className="flex items-center md:items-start justify-center md:justify-start gap-3 my-2 colorScroll">
-          {product?.colors?.map((color) => (
-            <div
-              onClick={() => setOptions({ ...options, ColorChoice: color })}
-              key={color}
-              className={`${
-                options.ColorChoice.length > 0 &&
-                options.ColorChoice === color &&
-                "scale-[1.4]"
-              } w-7 md:w-4 h-7 md:h-4 rounded-full active:scale-[.93] cursor-pointer before:absolute before:content-[""] md:before:w-5 md:before:h-5 before:h-8 before:w-8 before:border-solid before:border-[1.4px] before:rounded-full before:-top-[2px] before:-left-[2px] before:border-gray-300 relative`}
-              style={{ backgroundColor: color, borderColor: color }}
-            ></div>
-          ))}
+        <div className="w-full flex items-center justify-between">
+          <h1 className="font-Roboto my-1 font-semibold">{product.title}</h1>
         </div>
         <div className="w-full flex items-center justify-start gap-1">
           {product?.categories?.map((cat, idx) => (
